c64685c237182b265a8fef6cef64f7ad3763ebab
Gabriel Pinheiro <gabrielpsilvas@live.com>
2022-04-02 15:40:15 -0300

    feat: added styled-components

---
+++ src/screens/home/index.tsx
@@ -0,0 +1,9 @@
+import * as S from './styles'
+
+export function Home () {
+  return (
+    <S.Wrapper>
+      <S.Text>Home Screen</S.Text>
+    </S.Wrapper>
+  )
+}
---
+++ src/screens/home/styles.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components/native'
+
+export const Wrapper = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`
+
+export const Text = styled.Text`
+  color: greenyellow;
+  font-size: 24px;
+  font-weight: bold;
+`
