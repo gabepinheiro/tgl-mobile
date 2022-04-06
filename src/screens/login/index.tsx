5178168091fbcb038c2d8bf0042127039de814f2
Gabriel Pinheiro <gabrielpsilvas@live.com>
2022-04-06 14:43:43 -0300

    feat(screens/login): added controlled input, useForm and validations

--- src/screens/login/index.tsx
+++ src/screens/login/index.tsx
@@ -1,38 +1,70 @@
 import type { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { RootStackParamList } from '~/types'
+import { yupResolver } from '@hookform/resolvers/yup'
+import * as yup from "yup"
 
 import { AuthStackLayout } from '~/layouts'
-import { FormContainer, Input, ButtonLink, Center } from '~/components'
+import { FormContainer, ButtonLink, Center, ControlledInput } from '~/components'
 import { Feather } from '@expo/vector-icons'
 
 import { theme } from '~/styles'
 import * as S from './styles'
+import { useForm } from 'react-hook-form';
 
 type LoginProps =
   NativeStackScreenProps<RootStackParamList, 'Login'>
 
+type FormData = {
+  email: string
+  password: string
+}
+
+const schema = yup.object({
+  email: yup.string().email('E-mail inválido').required('Informe o e-mail'),
+  password: yup.string().min(6, 'A senha deve ter ao menos 6 dígitos')
+    .required('Informe a senha'),
+});
+
 export function Login ({ navigation }: LoginProps) {
+  const {
+    control,
+    handleSubmit,
+    formState: { errors }
+  } = useForm<FormData>({
+    resolver: yupResolver(schema)
+  })
+
   const navigateRegisterHandler = () => {
     navigation.navigate('Register')
+  }
+
+  const loginUserHandler = (data: FormData) => {
+
   }
 
   return (
     <AuthStackLayout heading='Authentication'>
       <S.Container>
         <FormContainer>
-          <Input
+          <ControlledInput
+            name='email'
+            control={control}
+            error={errors.email}
             placeholder='Email'
             keyboardType='email-address'
             autoCapitalize='none'
           />
-          <Input
+          <ControlledInput
+            name='password'
+            control={control}
+            error={errors.password}
             placeholder='Password'
             secureTextEntry
           />
           <Center style={{marginVertical: 32}}>
             <ButtonLink
               color='greenLight'
-              onPress={() => {console.log('Log in')}}
+              onPress={handleSubmit(loginUserHandler)}
               icon={<Feather
                 name='arrow-right'
                 size={24}
