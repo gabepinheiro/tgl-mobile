7789fb9acef2350c06d0de15ecdf2a824de6bbc6
Gabriel Pinheiro <gabrielpsilvas@live.com>
2022-04-07 11:42:24 -0300

    feat(storage): added removeItem method

--- src/storage/index.ts
+++ src/storage/index.ts
@@ -2,16 +2,27 @@ import AsyncStorage from '@react-native-async-storage/async-storage'
 
 export const Storage = {
   async setItem(key: string, value: any) {
-    await AsyncStorage.setItem(key, JSON.stringify(value))
+    try {
+      await AsyncStorage.setItem(key, JSON.stringify(value))
+    } catch (error) {}
   },
 
   async getItem<T>(key: string): Promise<T | null> {
-    const value = await AsyncStorage.getItem(key)
+    try {
+      const value = await AsyncStorage.getItem(key)
 
-    if(value) {
-      return JSON.parse(value)
-    }
+      if(value) {
+        return JSON.parse(value)
+      }
+
+    } catch (error) {}
 
     return null
+  },
+
+  async removeItem (key: string) {
+    try {
+      await AsyncStorage.removeItem(key)
+    } catch (error) {}
   }
 }
