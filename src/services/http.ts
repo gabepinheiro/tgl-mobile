6aeaf6cf4349d6e65c9dd774afa271ed1d1272ac
Gabriel Pinheiro <gabrielpsilvas@live.com>
2022-04-07 12:24:44 -0300

    feat(services/http): added interpector to request

--- src/services/http.ts
+++ src/services/http.ts
@@ -1,5 +1,21 @@
 import axios from 'axios'
+import { Storage } from '~/storage'
+import { AuthenticatedUser } from '~/types'
 
 export const api = axios.create({
   baseURL: 'http://localhost:3333'
+})
+
+api.interceptors.request.use(async (config) => {
+  const auth = await Storage.getItem<AuthenticatedUser>('@auth')
+
+  if (!auth) {
+    return config
+  }
+
+  if (config.headers) {
+    config.headers['Authorization'] = `Bearer ${auth.token.token}`
+  }
+
+  return config
 })
